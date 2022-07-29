e1617498787bb1f30a97f4f46b601ed823dac8ec
Prabhjot Singh <Singhprabhjot6108@gmail.com>
2022-07-28 21:42:12 -0600

    add roast_koko command

--- bot.py
+++ bot.py
@@ -2,18 +2,34 @@
 import os
 
 import discord
+from discord.ext import commands
+import requests
 # from dotenv import load_dotenv
 
 # load_dotenv()
-TOKEN = 'MTAwMTMxMzAzNTM5NjMxNzI0NA.GKCu6C.KYnKTfSxef2daWYB-66oFxnq35fl5KDpxmP_uw'
+TOKEN = 'test_token'
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='~', description="test bot", intent=discord.Intents.default())
 
-@client.event
+@bot.event
 async def on_ready():
-    activity = discord.Game(name=":help  for commands")
-    await client.change_presence(status=discord.Status.online, activity=activity)
     print ('bot is ready')
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
-client.run(TOKEN)
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! Responded in {round(bot.latency * 1000)}ms')
+
+@bot.command()
+async def roast_koko(ctx):
+    response = requests.get("https://insult.mattbas.org/api/insult")
+    await ctx.send(response.text) 
+
+# @bot.event
+# async def on_message():
+#     activity = discord.Game(name=":help  for commands")
+#     await bot.change_presence(status=discord.Status.online, activity=activity)
+#     print ('bot is ready')
+#     print('We have logged in as {0.user}'.format(bot))
+
+bot.run(TOKEN)
